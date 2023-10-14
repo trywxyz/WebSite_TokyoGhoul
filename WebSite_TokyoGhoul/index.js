b8166c8596f16d0b72758a26cc8ab4f64efdc2c0
Try <127165831+tryzwk@users.noreply.github.com>
2023-10-14 20:50:13 -0300

    Delete WebSite_TokyoGhoul directory

--- WebSite_TokyoGhoul/index.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('button')
-const navigation = document.querySelector('.nav')
-
-function handleClick() {
-  button.classList.toggle('active')
-  navigation.classList.toggle('active')
-}
